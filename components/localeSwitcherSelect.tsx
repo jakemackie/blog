66efead44cbd8ec4dd4911cbb78586eeeb2adad8
Jake Mackie <jake@hiyield.co.uk>
2024-10-03 08:38:50 +0100

    hotfix: Button disabled during state change

--- components/localeSwitcherSelect.tsx
+++ components/localeSwitcherSelect.tsx
@@ -39,8 +39,13 @@ export default function LocaleSwitcherSelect({
             <select
                 id="locale"
                 defaultValue={defaultValue}
-                className={`mt-2 block w-full rounded-md bg-background border-0 py-1.5 pl-3 pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+                className={
+                    `mt-2 block w-full rounded-md bg-background border-0 py-1.5 pl-3 
+                    pr-10 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 
+                    sm:text-sm sm:leading-6`
+                }
                 onChange={onSelectChange}
+                disabled={isPending}
             >
                 {children}
             </select>
