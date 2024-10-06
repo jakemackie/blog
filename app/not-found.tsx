fe9d34d861f719a7c02cad4c508a0248e0ee9408
jakemackie <jake.wmackie@gmail.com>
2024-10-06 19:30:23 +0100

    feat: Super quick not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+      <h2 className='text-4xl font-bold'>404</h2>
+      <h3 className='text-lg font-medium'>Page not found</h3>
+    </div>
+  );
+}
