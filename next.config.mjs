440e4eefa8bf22ce6a9a53a0ae8ac44b1da5ea6a
Jake Mackie <jake@hiyield.co.uk>
2024-10-07 14:21:41 +0100

    amend: Move from deprecated nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,17 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  images: { domains: ['cdn.sanity.io'] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/images/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
